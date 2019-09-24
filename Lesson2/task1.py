06c5121eae9f26ddd9d4a99e199dd88be7c076db
dctetsenko <stetdim1@gmail.com>
2019-09-24 11:31:06 +0300

    Start1
    Signed-off-by: dctetsenko <stetdim1@gmail.com>

--- Lesson2/task1.py
+++ Lesson2/task1.py
@@ -1,6 +1,22 @@
+import re
 print('start program')
+# f = open('test1.txt', 'r')#, encoding='utf-16')
+# s = f.readlines()
+# # lines = list(s)
+# print(s)
+# f.close()
 
-with f as open("test1.txt", 'r'):
+with open('test1.txt', 'r+', encoding='utf-8') as file:
     while True:
-        s=f.readline()
+        #s = file.readline()
+        s = file.read()
+        if s != "":
+            print(s)
+            sn = re.sub(r' ', r'', s)
+            print(sn)
+        else:
+            print('Break')
+            file.close() #not nesessery with 'with'
+            break
+
 
